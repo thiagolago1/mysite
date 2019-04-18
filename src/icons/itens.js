52fb2ccb046f441bd32e94f9ef665f7e4a642e84
Thiago Lago <thiagolagoo2@gmail.com>
2019-04-17 23:50:28 -0300

    [ADD] Font Awesome Icons

---
+++ src/icons/itens.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Icon from './icon'
+
+const IconHome = () => (
+    <Icon icon="home" />
+)
+
+const IconBars = () => (
+    <Icon icon="bars" />
+)
+
+export { IconHome, IconBars }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faHome, faBars } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faHome)
+library.add(faBars)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
