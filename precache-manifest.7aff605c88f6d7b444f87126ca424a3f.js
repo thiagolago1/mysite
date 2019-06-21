33f796ecdc3c383024b944239692854eed68205d
Thiago <thiagolagoo2@gmail.com>
2019-06-21 19:01:24 -0300

    Updates

---
+++ precache-manifest.7aff605c88f6d7b444f87126ca424a3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f55b66cf591ea219fdeebde62e937429",
+    "url": "/index.html"
+  },
+  {
+    "revision": "695b6c634e9377578fbf",
+    "url": "/static/css/main.87ee858a.chunk.css"
+  },
+  {
+    "revision": "65d19e19af502909da12",
+    "url": "/static/js/2.3671c937.chunk.js"
+  },
+  {
+    "revision": "695b6c634e9377578fbf",
+    "url": "/static/js/main.c1f4a35b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "49bf3b986843e53930671f098601c8bb",
+    "url": "/static/media/codeimage.49bf3b98.jpg"
+  }
+]);
